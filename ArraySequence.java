f3567eba6cce305a490a9ccb4c115083a74544a2
mmelucci <mmelucci20@stuy.edu>
2020-12-07 13:13:29 -0500

    Finished the first constructor and test

--- ArraySequence.java
+++ ArraySequence.java
@@ -4,22 +4,44 @@ public class ArraySequence implements IntegerSequence{
   int []data;
 
   /*Construct the sequence by copying values from the other array into the data array*/
-  public ArraySequence(int [] other){
-    this.currentIndex = 0;
-    for (int i = 0; i < other.length; i ++) {
-      this.data[i] = other[i];
+  public ArraySequence(int[] other){
+    data = other;
+    currentIndex = 0;
+  }
+
+  //Postcondition: The otherseq will be reset.
+  //This constructor will copy ALL values of the otherseq into the data array.
+  // public ArraySequence(IntegerSequence otherseq){
+  // }
+
+  public boolean hasNext(){
+    return currentIndex < length();
+  }
+
+  public int next(){
+    if (hasNext()==false){
+      throw new NoSuchElementException("There is no element next");
+    }
+    currentIndex++;
+    return data[currentIndex-1];
+  }
+
+  public int length(){
+    return data.length;
+  }
+
+  public void reset(){
+    currentIndex = 0;
+  }
+
+  public static void main(String[]args){
+    int[]nums = {1,3,5,0,-1,3,9};
+    IntegerSequence as = new ArraySequence(nums);
+
+    System.out.print("ArraySequence(array):");
+    while(as.hasNext()){
+      System.out.print(as.next()+", ");
     }
   }
-  public void reset () {
-    this.currentIndex = 0;
-  }
-  public int length(){
-    return 0;
-  }
-  public boolean hasNext(){
-    return true;
-  }
-  public int next(){
-    return 0;
-  }
+
 }
