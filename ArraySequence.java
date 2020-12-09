b5eae1d73f9cc78d44d8a60777925b59e8bf14fc
mmelucci <mmelucci20@stuy.edu>
2020-12-08 22:55:49 -0500

    Fixed the first constructor to account for NullPointerException

--- ArraySequence.java
+++ ArraySequence.java
@@ -5,17 +5,28 @@ public class ArraySequence implements IntegerSequence{
 
   /*Construct the sequence by copying values from the other array into the data array*/
   public ArraySequence(int[] other){
-    data = other;
+    data = new int[other.length];
+    for (int i = 0; i < other.length; i ++) {
+      data[i] = other[i];
+    }
     currentIndex = 0;
   }
 
   //Postcondition: The otherseq will be reset.
   //This constructor will copy ALL values of the otherseq into the data array.
   // public ArraySequence(IntegerSequence otherseq){
+  //   data = new int[otherseq.length()];
+  //   while
+  //   for (int i = 0; i < otherseq.length(); i ++) {
+  //     data[i] = otherseq[i];
+  //   }
   // }
 
   public boolean hasNext(){
-    return currentIndex < length();
+    if (this.currentIndex == data.length) {
+      return false;
+    }
+    return true;
   }
 
   public int next(){
@@ -38,7 +49,7 @@ public class ArraySequence implements IntegerSequence{
     int[]nums = {1,3,5,0,-1,3,9};
     IntegerSequence as = new ArraySequence(nums);
 
-    System.out.print("ArraySequence(array):");
+    System.out.println("ArraySequence(array):");
     while(as.hasNext()){
       System.out.print(as.next()+", ");
     }
